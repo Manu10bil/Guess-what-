104b8f6e4e328120a0a0fe99ea297a7284377b1b
Manu10bil <v7yn8fw7hs@privaterelay.appleid.com>
2026-01-31 01:19:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ yesBtn.addEventListener("click", () => {
     <div style="display:flex;flex-direction:column;justify-content:center;align-items:center;height:100vh;background:#ff9a9e;">
       <h1 style="color:white;font-size:3rem;">YUHUUUUUU I love you Babyyyyy!</h1>
       <p style="color:white;font-size:1.5rem;">Wirklich danke, dass ich dich habeeee ich bin wirklich so frohhh. Du bist das allerbeste was mir je passiert ist ich freue mich schon voll auf den 14. Heheeeeee.</p>
-      <!-- Corner images -->
+
       <img src="image.jpg" class="corner top-left">
       <img src="image (1).jpg" class="corner top-right">
       <img src="image (2).jpg" class="corner bottom-left">
